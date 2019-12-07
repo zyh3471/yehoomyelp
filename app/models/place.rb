dff6fe77e2ec0bd337f7d268dddf49d0cb82f55d
Yehoom <zjzzhou@ucdavis,edu>
2019-12-07 07:43:06 +0000

    12/6

---
+++ app/models/place.rb
@@ -0,0 +1,9 @@
+class Place < ApplicationRecord
+    belongs_to :user
+    has_many :comments
+    
+    geocoded_by :address
+    after_validation :geocode
+    
+    validates :name, presence: true
+end
---
+++ db/migrate/20190905013310_add_new_field.rb
@@ -0,0 +1,6 @@
+class AddNewField < ActiveRecord::Migration[5.2]
+  def change
+  add_column :photos, :caption, :text
+  add_column :photos, :place_id, :integer
+  end
+end
