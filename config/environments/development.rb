a773bda17b650f9047c87fe421302180bd15d5a9
Yehoom <zjzzhou@ucdavis,edu>
2019-12-08 12:49:14 +0000

    12/8

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
 
+  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
