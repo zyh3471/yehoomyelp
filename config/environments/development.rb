ab043201a730ded2b8918bf28a736bed510fa7b4
Yehoom <zjzzhou@ucdavis,edu>
2019-12-11 09:20:21 +0000

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: 'localhost:3030'}
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :test
+ # config.action_mailer.delivery_method = :test
 
   
 
