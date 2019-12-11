63a68ea77b6e1405dfdba90fc784a84581cb25e7
Yehoom <zjzzhou@ucdavis,edu>
2019-12-11 00:53:13 +0000

    emailok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,16 +1,21 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
-
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
   }
 
-  # more code here
-end
+
+  config.action_mailer.default_url_options = {host: 'localhost:3030'}
+  config.action_mailer.raise_delivery_errors = false
+
+  
+
+  # more code here....
+ end
