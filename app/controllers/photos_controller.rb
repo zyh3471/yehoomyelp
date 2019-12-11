40b32ab4329ddaf727e6f7b2db31140f421373b6
Yehoom <zjzzhou@ucdavis,edu>
2019-12-11 10:57:00 +0000

    123

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,6 @@ class PhotosController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:picture, :caption)
+    params.require(:photo).permit(:picture)
   end
  end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
-class Photo < ActiveRecord::Base
-  mount_uploader :picture, PictureUploader
+class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  mount_uploader :picture, PictureUploader
 end
