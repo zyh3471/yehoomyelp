2ef0fd48fc731bb587d728a3be2b480a55efe0c5
yehoom <zjzzhou@ucdavis.edu>
2019-12-19 00:20:29 +0000

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-
+  config.action_mailer.default_url_options = { host: 'yehoomyelp.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   
